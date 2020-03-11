d8de49d9585e3fb62f3a1f71e0bd0b807c1574d3
tyson boren <tyson.d.boren@gmail.com>
2020-03-11 11:31:28 -0600

    added a form to create todo and updated state with the value and added basic styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./styles.css"
 
 class App extends React.Component {
     constructor() {
         super()
 
+        this.state = {
+            todo: "",
+        }
+    }
+
+
+    handleChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            todo: event.target.value
+        })
+    }
+
+    addTodo = (event) => {
+        console.log('I added a todo')
+        event.preventDefault()
     }
 
 
@@ -12,7 +29,17 @@ class App extends React.Component {
         return (
             <div className="app">
                 <h1>Todo List</h1>
-                <div></div>
+                        
+                <form className="add-todo" onSubmit={this.addTodo}>
+                    <input
+                        type="text"
+                        placeholder="Add Todo"
+                        value={this.state.todo}
+                        onChange={this.handleChange}
+                    />
+
+                    <button type='submit'>Add</button>
+                </form>
             </div>
         )
     }
